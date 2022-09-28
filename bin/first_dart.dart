931d274165231fdd44c0a496a31d7245c997bdd2
MB Phong Ram <mabuphongram171@gmail.com>
2022-09-28 05:52:20 -0400

    Collection of list,map,set

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -1,18 +1,26 @@
 import 'package:first_dart/first_dart.dart' as first_dart;
 
 void main(List<String> arguments) {
- 
-  print('${first_dart.calculate()}!');
- 
-  
- final twicePulsFive = twice((x) => x+5);
- final result = twicePulsFive(3);
-print(result);
-}
+ // #first way
+ List<int> myList = [1,2,3,4,5];
+ final firstElement = myList[1];
 
-typedef IntTransfomer =int Function(int);
- IntTransfomer twice(int Function(int) f){
-  return(int x){
-    return f(f(x));
-  };  
+  print('${first_dart.calculate()}!');
+
+//third way
+ <int> [1,2,3,4,5];
+
+ //using map
+ Map<String, dynamic> myMap = {
+  'name':'Jhon Doe',
+  'age' : 20,
+  'registered': true
+ };
+ final myName = myMap['name'];
+print(myName);
+
+//using set 
+//it opmit the same find
+Set<int> myset = {1,2,1,4,5};
+print(myset.length);
 }
