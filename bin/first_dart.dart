d52a859ba9e129114d16c5453396917b03bece65
MB Phong Ram <mabuphongram171@gmail.com>
2022-09-28 05:24:52 -0400

    twice function

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -3,30 +3,14 @@ import 'package:first_dart/first_dart.dart' as first_dart;
 void main(List<String> arguments) {
  
   print('${first_dart.calculate()}!');
-
- // greeting parameter can be optional
-  void positionalParams(int width, int height, [String? greeting]){
-    positionalParams(4,6);
-  }
-
-//void positionalOptionalParams
-void positionalOptionalParams({
-  int? x,
-  int? y,
-  String? greeting
-}){
-  positionalOptionalParams(x: 19,greeting: 'hello world');
-}
-
-// requiredParameter
-void requiredParameter( int positional,
-{
-  required int x,
-  required int y,
-  required String greeting
-}){
-requiredParameter(10, x: 34, y: 65, greeting: 'Welcome to mobile legends');
-}
-
  
+  
+ final twicePulsFive = twice((x) => x+5);
+ final result = twicePulsFive(3);
+print(result);
+}
+int Function(int) twice(int Function(int) f){
+  return(int x){
+    return f(f(x));
+  };
 }
