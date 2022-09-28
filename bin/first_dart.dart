9156a19be7e4cf1143b28a6567c647358e8ac5bc
MB Phong Ram <mabuphongram171@gmail.com>
2022-09-28 08:39:31 -0400

    enum

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -1,21 +1,16 @@
-
+enum AccountType {free,premium,vip}
 void main(List<String> arguments) {
-bool isSignedIn = true;
-<String> [
-  'This is a fake account',
-  if(isSignedIn) 'Sign out' else 'Sign In',
-];
+final userAccountType = AccountType.premium;
+print(userAccountType.index);
 
-final x = <String>[
-  for (int i =0;i< 5;i++) i.toString(),
-  for(final number in [1,2,3,4]) number.toString()
-];
 
-final list1 = ['hello','there'];
-final list2 = ['what','up'];
-final y = <String>[
-  ...list1,
-  ...list2
-];
-print(y);
+switch (userAccountType) {
+  case AccountType.free:
+    print("0 USD!");
+    break;
+    case AccountType.premium:
+    print("20 USD!");
+    break;
+  default:break;
+}
 }
