489a0cb744c6093f3de245f4cb7497f4eaf129c7
MB Phong Ram <mabuphongram171@gmail.com>
2022-09-28 04:32:05 -0400

    operator

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -1,19 +1,18 @@
 import 'package:first_dart/first_dart.dart' as first_dart;
 
 void main(List<String> arguments) {
-  String myString = 'Welcome to Mobile Legends';
-  print('${myString} ${first_dart.calculate()}!');
-  print(myString.contains('Welcome'));
-  int myInteger = 5;
-  print(myInteger.toString());
-  print(myInteger.isEven);
-  double myDouble = 5.5;
+ 
+  print('${first_dart.calculate()}!');
 
-// this is new syntax
-  num myNum = 5.5;
+int result;
+ double resultDouble = 5/5;
+ result=  5~/5; // it truncate decimal point
+ print(result);
+ print(resultDouble);
 
- // dynamic can be any type of data type
-  dynamic mySomething = 5;
-  mySomething = 'hello';
-  mySomething = true;
+ bool isEqual = 5==10;
+ print(isEqual);
+
+
+ 
 }
