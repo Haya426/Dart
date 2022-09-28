2d39be7b6a9cdd423e07d5cf820461e0a4ad73b5
MB Phong Ram <mabuphongram171@gmail.com>
2022-09-28 04:53:40 -0400

    function

--- bin/first_dart.dart
+++ bin/first_dart.dart
@@ -4,15 +4,29 @@ void main(List<String> arguments) {
  
   print('${first_dart.calculate()}!');
 
-int result;
- double resultDouble = 5/5;
- result=  5~/5; // it truncate decimal point
- print(result);
- print(resultDouble);
+ // greeting parameter can be optional
+  void positionalParams(int width, int height, [String? greeting]){
+    positionalParams(4,6);
+  }
 
- bool isEqual = 5==10;
- print(isEqual);
+//void positionalOptionalParams
+void positionalOptionalParams({
+  int? x,
+  int? y,
+  String? greeting
+}){
+  positionalOptionalParams(x: 19,greeting: 'hello world');
+}
 
+// requiredParameter
+void requiredParameter( int positional,
+{
+  required int x,
+  required int y,
+  required String greeting
+}){
+requiredParameter(10, x: 34, y: 65, greeting: 'Welcome to mobile legends');
+}
 
  
 }
